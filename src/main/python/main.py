698cbb93799d2636aa743be6e964076d2cad9b34
RiviereGregory <gregory.riviere@laposte.net>
2020-12-19 15:52:52 +0100

    Ajout création des Actions et des icones

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
     window = MainWindow()
-    window.resize(500, 400)
+    window.resize(1920 / 2, 1200 / 2)
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
--- src/main/python/package/main_window.py
+++ src/main/python/package/main_window.py
@@ -5,6 +5,13 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyPlayer")
+
+        self.open_icon = self.style().standardIcon(QtWidgets.QStyle.SP_DriveDVDIcon)
+        self.play_icon = self.style().standardIcon(QtWidgets.QStyle.SP_MediaPlay)
+        self.previous_icon = self.style().standardIcon(QtWidgets.QStyle.SP_MediaSkipBackward)
+        self.pause_icon = self.style().standardIcon(QtWidgets.QStyle.SP_MediaPause)
+        self.stop_icon = self.style().standardIcon(QtWidgets.QStyle.SP_MediaStop)
+
         self.setup_ui()
 
     def setup_ui(self):
@@ -19,6 +26,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.player = QtMultimedia.QMediaPlayer()
         self.toolbar = QtWidgets.QToolBar()
         self.file_menu = self.menuBar().addMenu("Fichier")
+
+        # ACTIONS
+        self.act_open = self.file_menu.addAction(self.open_icon, "Ouvrir")
+        self.act_open.setShortcut("Ctrl+O")
+        self.act_play = self.toolbar.addAction(self.play_icon, "Lire")
+        self.act_previous = self.toolbar.addAction(self.previous_icon, "Revenir au début")
+        self.act_pause = self.toolbar.addAction(self.pause_icon, "Pause")
+        self.act_stop = self.toolbar.addAction(self.stop_icon, "Stop")
 
     def modify_widgets(self):
         pass
